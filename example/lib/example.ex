fd2c9f12180fcabbe804580114efe108a61c6a23
Frank <Frankuik@hotmail.com>
2025-10-28 14:25:10 +0100

    1

--- example/lib/example.ex
+++ example/lib/example.ex
@@ -2,14 +2,20 @@ defmodule Example do
 use Application
 alias UUID
 def start(_type,_args)do
-
-IO.puts(UUID.uuid4())
+Example.main()
 Supervisor.start_link([], strategy: :one_for_one)
  end
 
 
-def hello do
-:hej
+def main do
+
+name = "Frank"
+status = Enum.random([:gold, :"not a member"])
+ case status do
+  :gold -> IO.puts("welcome to the fancy lounge, #{name}")
+  :"not a member" -> IO.puts("lol get out ")
+ _ -> IO.puts("get out skkrtboy")
+end
 end
 
 
