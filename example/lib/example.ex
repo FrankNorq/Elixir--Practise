522eba399d19769f70de5c5f1cd78e8acbf2fb23
Frank <Frankuik@hotmail.com>
2025-10-29 10:32:52 +0100

    days until newyear

--- example/lib/example.ex
+++ example/lib/example.ex
@@ -8,12 +8,20 @@ Supervisor.start_link([], strategy: :one_for_one)
 
 
 def main do
-# IO.puts("interpolation looks like \#{}")
-a = 10
-a = a + 5.0
-b = 3.7
-IO.puts(a+b)
+time = DateTime.new!(Date.new!(2026,1,1),Time.new!(0,0,0,0), "Etc/UTC")
+time_till = DateTime.diff(time,DateTime.utc_now())
+IO.puts(time_till)
+days = div(time_till , 86400)
+IO.puts(days)
+hours = div(rem(time_till,86_400),60 *60)
+IO.puts(hours)
+minutes = div(rem(time_till,60 *60),60)
+IO.puts(minutes)
+secounds = rem(time_till,60)
+IO.puts(secounds)
+IO.puts("Time until new year: #{days} days, #{hours} hours, #{minutes} minutes, #{secounds} secounds.")
 end
+
 
 
 end
