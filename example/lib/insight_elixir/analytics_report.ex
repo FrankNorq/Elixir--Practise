7e23e12125798bc97223728c55cb85b745339057
Frank <Frankuik@hotmail.com>
2025-11-04 13:09:27 +0100

    shopping cart

--- example/lib/insight_elixir/analytics_report.ex
+++ example/lib/insight_elixir/analytics_report.ex
@@ -2,17 +2,27 @@ defmodule InsightElixir.AnalyticsReport do
   @moduledoc """
   This module handles the generation of reports
   """
+  import InsightElixir.Utilities, only: [format_date: 1]
+  alias Calculations.EngagementCalculator, as: Calc
+  require InsightElixir.ReportMacros
+
   @constant_value 22
   def generate_report(:user_engagement, :weekly) do
-    "user engagement report generated bajs"
+    formatted_date = format_date(DateTime.utc_now())
+    "user engagement weekly report for #{formatted_date}"
   end
 
   def generate_report(:sales, :monthly) do
-    "monthly sales reports"
+    engagement_calc = Calc.calculate_engagement()
+    "monthly sales reports generated with #{engagement_calc}"
   end
 
   def generate_report(:traffic, range = %{from: _, to: _}) do
     "traffic report generated for range #{inspect(range)}."
+  end
+
+  def generate_report(:user_engagement) do
+    ReportMacros.log_report_generation(:user_engagement)
   end
 
   # generate report
---
+++ shopping_cart/lib/shopping_cart.ex
@@ -0,0 +1,38 @@
+defmodule ShoppingCart do
+  @moduledoc """
+  Documentation for `ShoppingCart`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ShoppingCart.hello()
+      :world
+
+  """
+  def add_item(cart, item = %{name: _, price: _}) do
+    [item | cart]
+  end
+
+  def remove_item(cart, item = %{name: _, price: _}) do
+    List.delete(cart, item)
+  end
+
+  def remove_firt([_head | tail]) do
+    tail
+  end
+
+  def get_first(cart) do
+    List.first(cart)
+  end
+
+  def get_last(cart) do
+    List.last(cart)
+  end
+
+  def get_total(cart) do
+    List.foldl(cart, 0, fn item, total -> item(price + total) end)
+  end
+end
